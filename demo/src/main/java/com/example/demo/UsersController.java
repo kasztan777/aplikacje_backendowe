0155fea597801b42fcf1b6a55095f49dd1984e3d
kasztan777 <91385175+kasztan777@users.noreply.github.com>
2022-04-10 17:52:02 +0200

    Add files via upload

--- demo/src/main/java/com/example/demo/UsersController.java
+++ demo/src/main/java/com/example/demo/UsersController.java
@@ -7,10 +7,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsersController {
-    @RequestMapping("/users")
+    private Map<Integer, UserEntity> users
+
+    @RequestMapping("api/users")
     @ResponseBody
-    public Object getUsers() {
-        @RequestParam Integer pageNumber;
-        @RequestParam Integer pageSize;
+    public Object getUsers(
+            @RequestParam (name = "page-number", defaultValue = "1") Integer pageNumber,
+            @RequestParam (name = "page-size", defaultValue = "20") Integer pageSize,
+
+            ) {
+        //@RequestParam Integer pageNumber;
+        //@RequestParam Integer pageSize;
+
+        UsersPage page = new UsersPage();
+
+        page.setPageNumber(pageNumber);
+        page.setPagesCount(1);
+        page.setPageSize(pageSize);
+        page.setTotalCount(1);
+        page.getUsers(this.users.values())
+        return page;
     }
 }
---
+++ demo/src/main/java/com/example/demo/UsersPage.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.util.Collection;
+
+public class UsersPage {
+    private int pageNumber;
+    private int pageCount;
+    private int pageSize;
+    private int totalCount;
+    private Collection<UserEntity> users;
+
+}
